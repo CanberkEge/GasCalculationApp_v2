dbfd087197d16a894d7109ed85c24c8bef2aa303
CanberkEge <canberk2396@hotmail.com>
2023-11-16 20:31:03 +0300

    Add project files.

---
+++ GasCalculationApp_v2/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace GasCalculationApp_v2
+{
+    partial class GasCalculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.distanceTextBox = new System.Windows.Forms.TextBox();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.averageGasSpendTextBox = new System.Windows.Forms.TextBox();
+            this.daysNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.distanceLabel = new System.Windows.Forms.Label();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.avgGasSpendLabel = new System.Windows.Forms.Label();
+            this.timesLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.daysNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // distanceTextBox
+            // 
+            this.distanceTextBox.Location = new System.Drawing.Point(283, 134);
+            this.distanceTextBox.Name = "distanceTextBox";
+            this.distanceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.distanceTextBox.TabIndex = 0;
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Location = new System.Drawing.Point(283, 181);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.priceTextBox.TabIndex = 1;
+            // 
+            // averageGasSpendTextBox
+            // 
+            this.averageGasSpendTextBox.Location = new System.Drawing.Point(283, 223);
+            this.averageGasSpendTextBox.Name = "averageGasSpendTextBox";
+            this.averageGasSpendTextBox.Size = new System.Drawing.Size(100, 20);
+            this.averageGasSpendTextBox.TabIndex = 2;
+            // 
+            // daysNumericUpDown
+            // 
+            this.daysNumericUpDown.Location = new System.Drawing.Point(283, 296);
+            this.daysNumericUpDown.Name = "daysNumericUpDown";
+            this.daysNumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.daysNumericUpDown.TabIndex = 3;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.BackColor = System.Drawing.Color.Yellow;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.resultLabel.ForeColor = System.Drawing.Color.Red;
+            this.resultLabel.Location = new System.Drawing.Point(583, 71);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(68, 16);
+            this.resultLabel.TabIndex = 4;
+            this.resultLabel.Text = "Sonuçlar";
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(86, 37);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(75, 23);
+            this.CalculateButton.TabIndex = 5;
+            this.CalculateButton.Text = "Hesapla";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click_1);
+            // 
+            // distanceLabel
+            // 
+            this.distanceLabel.AutoSize = true;
+            this.distanceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.distanceLabel.Location = new System.Drawing.Point(79, 138);
+            this.distanceLabel.Name = "distanceLabel";
+            this.distanceLabel.Size = new System.Drawing.Size(58, 16);
+            this.distanceLabel.TabIndex = 6;
+            this.distanceLabel.Text = "Mesafe";
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.priceLabel.Location = new System.Drawing.Point(79, 185);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(120, 16);
+            this.priceLabel.TabIndex = 7;
+            this.priceLabel.Text = "Benzin litre fiyatı";
+            // 
+            // avgGasSpendLabel
+            // 
+            this.avgGasSpendLabel.AutoSize = true;
+            this.avgGasSpendLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.avgGasSpendLabel.Location = new System.Drawing.Point(79, 227);
+            this.avgGasSpendLabel.Name = "avgGasSpendLabel";
+            this.avgGasSpendLabel.Size = new System.Drawing.Size(171, 16);
+            this.avgGasSpendLabel.TabIndex = 8;
+            this.avgGasSpendLabel.Text = "100km\'de harcanan litre";
+            // 
+            // timesLabel
+            // 
+            this.timesLabel.AutoSize = true;
+            this.timesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.timesLabel.Location = new System.Drawing.Point(79, 296);
+            this.timesLabel.Name = "timesLabel";
+            this.timesLabel.Size = new System.Drawing.Size(198, 16);
+            this.timesLabel.TabIndex = 9;
+            this.timesLabel.Text = "Kaç günlük sonuç isteniyor?";
+            // 
+            // GasCalculatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.timesLabel);
+            this.Controls.Add(this.avgGasSpendLabel);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.distanceLabel);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.daysNumericUpDown);
+            this.Controls.Add(this.averageGasSpendTextBox);
+            this.Controls.Add(this.priceTextBox);
+            this.Controls.Add(this.distanceTextBox);
+            this.Name = "GasCalculatorForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.daysNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox distanceTextBox;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.TextBox averageGasSpendTextBox;
+        private System.Windows.Forms.NumericUpDown daysNumericUpDown;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Label distanceLabel;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.Label avgGasSpendLabel;
+        private System.Windows.Forms.Label timesLabel;
+    }
+}
+
---
+++ GasCalculationApp_v2/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GasCalculationApp_v2
+{
+    public partial class GasCalculatorForm : Form
+    {
+        public GasCalculatorForm()
+        {
+            InitializeComponent();
+            InitializeTextBoxes();
+        }
+
+        private void InitializeTextBoxes()
+        {
+            SetFaintText(distanceTextBox, "Mesafeyi girin (km)");
+            SetFaintText(priceTextBox, "Yakıt litre fiyatını girin (tl)");
+            SetFaintText(averageGasSpendTextBox, "Ortalama tükeitimi girin (litre/100km)");
+
+            // Add event handlers to manage the faint text behavior
+            distanceTextBox.Enter += TextBox_Enter;
+            distanceTextBox.Leave += TextBox_Leave;
+
+            priceTextBox.Enter += TextBox_Enter;
+            priceTextBox.Leave += TextBox_Leave;
+
+            averageGasSpendTextBox.Enter += TextBox_Enter;
+            averageGasSpendTextBox.Leave += TextBox_Leave;
+        }
+
+        private void SetFaintText(TextBox textBox, string faintText)
+        {
+            textBox.ForeColor = SystemColors.GrayText;
+            textBox.Text = faintText;
+            textBox.Tag = faintText; // Set Tag property to store the original faint text
+        }
+
+        private void TextBox_Enter(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+
+            if (textBox.ForeColor == SystemColors.GrayText)
+            {
+                textBox.Text = "";
+                textBox.ForeColor = SystemColors.ControlText;
+            }
+        }
+
+        private void TextBox_Leave(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                SetFaintText(textBox, textBox.Tag.ToString());
+            }
+        }
+
+        
+
+        private void CalculateButton_Click_1(object sender, EventArgs e)
+        {
+            double distance, price, averageGasSpend, totalGasSpend, totalPrice;
+
+            if (!double.TryParse(distanceTextBox.Text, out distance) ||
+                !double.TryParse(priceTextBox.Text, out price) ||
+                !double.TryParse(averageGasSpendTextBox.Text, out averageGasSpend))
+            {
+                MessageBox.Show("Invalid input. Please enter valid numbers.");
+                return;
+            }
+
+            totalGasSpend = ((averageGasSpend * distance) / 100);
+            totalPrice = (totalGasSpend * price);
+
+            resultLabel.Text = $"Toplam harcanan yakıt = {totalGasSpend:F2} litre\nToplam tutar = {totalPrice:F2} tl";
+
+            double times;
+            string resume;
+            int days;
+
+            resume = MessageBox.Show("Kaç kez olduğunu belirtmek için devam mı?", "Devam?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes ? "devam" : "sonlanmıştır";
+
+            if (resume == "devam")
+            {
+                days = (int)daysNumericUpDown.Value;
+                times = (totalPrice * days);
+                resultLabel.Text += $"\n{days} günlük tutar = {times:F2} tl";
+            }
+            else
+            {
+                resultLabel.Text += "\nProgram sonlanmıştır.";
+            }
+        }
+    }
+}
